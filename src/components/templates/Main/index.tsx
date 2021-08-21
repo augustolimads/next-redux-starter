870576916b2b8a167e56659d534901c672c7353a
Augusto Lima <augustolimads@gmail.com>
2021-08-21 16:47:38 -0300

    add styled components

---
+++ src/components/templates/Main/index.tsx
@@ -0,0 +1,9 @@
+import S from './Main.module.scss'
+
+const Main = () => (
+  <div className={S.grid}>
+    <h1>Main</h1>
+  </div>
+)
+
+export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import { theme } from 'src/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Next Starter with redux and sass</title>
+        <link rel='shortcut icon' href='/img/icon-512.png' />
+        <link rel='apple-touch-icon' href='/img/icon-512.png' />
+        <link rel='manifest' href='/manifest.json' />
+        <meta
+          name='description'
+          content='A simple project starter to work with typescript, React, NextJs, Redux Toolkit and Sass+Css-modules'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 export default MyApp
