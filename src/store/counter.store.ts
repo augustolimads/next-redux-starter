bfc8c9c6166fc26d63b6257013fe4e71c4aa0f10
Augusto Lima <augustolimads@gmail.com>
2021-08-21 18:04:35 -0300

    refactor reducer example

--- src/store/counter.store.ts
+++ src/store/counter.store.ts
@@ -1,20 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+//reducers and actions examples
+
+import { createSlice } from '@reduxjs/toolkit'
 
 export const counterSlice = createSlice({
-  name: "counter",
+  name: 'counter',
   initialState: {
-    value: 0,
+    value: 0
   },
   reducers: {
     increment(state) {
-      state.value += 1;
+      state.value += 1
     },
     decrement(state) {
-      state.value -= 1;
-    },
-  },
-});
+      state.value -= 1
+    }
+  }
+})
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions
 
-export default counterSlice.reducer;
+export default counterSlice.reducer
